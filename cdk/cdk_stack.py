ae879e507f30f2ee63c68824ffa530cc8aa2be04
Hugh Wimberly <hugh.wimberly@gmail.com>
2021-03-05 12:55:52 -0800

    Remove SQS and SNS from demo stack

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -1,20 +1,7 @@
 from aws_cdk import aws_iam as iam
-from aws_cdk import aws_sns as sns
-from aws_cdk import aws_sns_subscriptions as subs
-from aws_cdk import aws_sqs as sqs
 from aws_cdk import core
 
 
 class CdkStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        queue = sqs.Queue(
-            self,
-            "CdkQueue",
-            visibility_timeout=core.Duration.seconds(300),
-        )
-
-        topic = sns.Topic(self, "CdkTopic")
-
-        topic.add_subscription(subs.SqsSubscription(queue))
--- tests/test_cdk_stack.py
+++ tests/test_cdk_stack.py
@@ -10,11 +10,3 @@ def get_template():
     app = core.App()
     CdkStack(app, "cdk")
     return json.dumps(app.synth().get_stack("cdk").template)
-
-
-def test_sqs_queue_created():
-    assert "AWS::SQS::Queue" in get_template()
-
-
-def test_sns_topic_created():
-    assert "AWS::SNS::Topic" in get_template()
