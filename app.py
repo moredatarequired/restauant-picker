f759197f8d733aa4c89c99e361900cfcb644b7e5
Hugh Wimberly <hugh.wimberly@gmail.com>
2021-03-08 16:33:58 -0800

    Use common environment for both stacks

--- app.py
+++ app.py
@@ -7,13 +7,11 @@ from cdk.restaurant_picker_stack import RestaurantPickerStack
 
 AWS_REGION = "us-west-2"
 AWS_ACCOUNT_ID = "724169626350"
+AWS_ENV = core.Environment(account=AWS_ACCOUNT_ID, region=AWS_REGION)
 
 app = core.App()
-RestaurantPickerStack(app, "restaurant-picker", env={"region": AWS_REGION})
 
-DeployPipelineStack(
-    app,
-    "deploy-pipeline",
-    env=core.Environment(account=AWS_ACCOUNT_ID, region=AWS_REGION),
-)
+RestaurantPickerStack(app, "restaurant-picker", env=AWS_ENV)
+DeployPipelineStack(app, "deploy-pipeline", env=AWS_ENV)
+
 app.synth()
