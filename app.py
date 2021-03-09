b2569f3dd5ac8d2b6cbc05d7b6207d7af13c0b8e
Hugh Wimberly <hugh.wimberly@gmail.com>
2021-03-08 16:34:36 -0800

    Resort name order

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from aws_cdk import core
 from cdk.deploy_pipeline_stack import DeployPipelineStack
 from cdk.restaurant_picker_stack import RestaurantPickerStack
 
-AWS_REGION = "us-west-2"
 AWS_ACCOUNT_ID = "724169626350"
+AWS_REGION = "us-west-2"
 AWS_ENV = core.Environment(account=AWS_ACCOUNT_ID, region=AWS_REGION)
 
 app = core.App()
